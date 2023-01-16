e259327989f45493080054f0947682d110ea533c
Dumble009 <daichi19990821@gmail.com>
2023-01-16 22:15:29 +0900

    mkdirを上手く認識しないので、build.shの先頭でPATHを再設定する処理を追加

--- .circleci/build.sh
+++ .circleci/build.sh
@@ -1,1 +1,6 @@
-﻿mkdir build; cd ./build; cmake ..; cd ../; cmake --build ./build --config Release --target all -j 8 --;
+﻿PATH=/bin:/usr/bin:/sbin:/usr/sbin:/usr/local/bin:/usr/local/sbin
+export PATH
+mkdir build
+cd ./build; cmake ..
+cd ../
+cmake --build ./build --config Release --target all -j 8 --
